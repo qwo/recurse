60e60f5112cb1d34f87cf687f363c070e38378f0
Stanley Zheng <Stanley@Zheng.nyc>
2019-11-05 19:15:35 -0500

    adding more from join page

--- sql/selectstarsql.sql
+++ sql/selectstarsql.sql
@@ -8,7 +8,9 @@ select
         END
     ) / COUNT(*)
 from
-    executions --- LongTail
+    executions 
+
+--- LongTail
 SELECT
     county,
     COUNT(*) AS county_executions
@@ -26,7 +28,8 @@ FROM
     executions
 group by
     county,
-    l -- Where before, having after
+    l 
+-- Where before, having after
 SELECT
     county
 FROM
@@ -36,7 +39,9 @@ where
 group by
     county
 having
-    count(*) > 2 -- The grouping columns don't necessarily have to be in the SELECT block.
+    count(*) > 2 
+    
+    -- The grouping columns don't necessarily have to be in the SELECT block.
     -- Even though it would be valid (in SQLite) for the reasons set forth in A Strange Query, it is poor form to have non-aggregate, non-grouping columns in the SELECT block.
 SELECT
     county,
@@ -46,7 +51,9 @@ FROM
     executions
 GROUP BY
     county,
-    decade_age --- Subquery under ( ) 
+    decade_age
+    
+ --- Subquery under ( ) 
 SELECT
     first_name,
     last_name
@@ -59,3 +66,30 @@ WHERE
         from
             executions
     )
+
+--- executions need to introspect 
+SELECT
+  county,
+  100.0 * COUNT(*) / (select count(*) from executions)
+    AS percentage
+FROM executions
+GROUP BY county
+ORDER BY percentage DESC
+
+--- Execution Hiatuses
+--- The JOIN command defaults to performing what is called an “inner join” in which unmatched rows are dropped.
+
+SELECT
+  last_ex_date AS start,
+  ex_date AS end,
+  JULIANDAY(ex_date) - JULIANDAY(last_ex_date)
+    AS day_difference
+FROM executions
+JOIN (select 
+	ex_number + 1 as ex_number,
+	ex_date  as last_ex_date
+from executions
+) previous
+  ON executions.ex_number = previous.ex_number
+ORDER BY day_difference DESC
+LIMIT 10
