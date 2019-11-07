8a82a87397886219eddf8cd4976eab5038b0f2fb
Stanley Zheng <Stanley@Zheng.nyc>
2019-11-07 18:42:36 -0500

    solved in SQL

---
+++ sql/mastermind/finished.sql
@@ -0,0 +1,68 @@
+-- Create Table
+-- Enforce if guess correctly
+-- Distinct combination
+DROP TYPE IF EXISTS color cascade ; CREATE TYPE color AS ENUM ('R', 'G', 'V', 'B', 'O', 'Y');
+
+DROP TABLE IF EXISTS guesses cascade ; CREATE TABLE guesses (
+    first color,
+    second color,
+    third color ,
+    fourth color
+);
+INSERT INTO mastermind values ('R', 'G', 'R', 'G');
+INSERT INTO guesses values ('R', 'G', 'G', 'R'); 
+INSERT INTO guesses values ('R', 'G', 'R', 'R');
+INSERT INTO guesses values ('R', 'G', 'R', 'G');
+INSERT INTO guesses values ('Y', 'G', 'R', 'G');
+CREATE TABLE mastermind () inherits (guesses);
+
+
+
+
+-- INSERT INTO guesses values ('R', 'R', 'R', 'Y')
+--- Matches Black
+
+CREATE OR REPLACE FUNCTION check_black(guesses,  mastermind) RETURNS integer AS $$
+    SELECT (
+        ($1.first  = $2.first)::int +
+        ($1.second  = $2.second)::int +
+        ($1.third = $2.third)::int +
+        ($1.fourth = $2.fourth)::int
+    );
+$$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION check_white(guesses,  mastermind) RETURNS integer AS $$
+    SELECT (
+
+        case when ($1.first != $2.first) then (
+                $1.first = $2.second or
+                $1.first = $2.third or
+                $1.first = $2.fourth
+                )::int else 0 end +
+        case when ($1.second != $2.second) then (
+                $1.second = $2.first or
+                $1.second = $2.third or
+                $1.second = $2.fourth
+                )::int else 0 end +
+        case when ($1.third != $2.third) then (
+                $1.third = $2.first or
+                $1.third = $2.second or
+                $1.third = $2.fourth
+                )::int else 0 end +
+        case when ($1.fourth != $2.fourth) then (
+                $1.fourth = $2.first or
+                $1.fourth = $2.second or
+                $1.fourth = $2.third
+                )::int else 0 end
+
+    );
+$$ LANGUAGE SQL;
+
+SELECT guesses,  check_black(guesses.*, mastermind.*), check_white(guesses.*, mastermind.*)
+    FROM guesses, mastermind
+
+/*SELECT count(coalesce(
+            guesses.* = mastermind.*, false
+        )) as black
+from guesses left join mastermind on guesses.* = mastermind.**/
+
